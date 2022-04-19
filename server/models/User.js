91ce0cb6a4df2fac7ffede3fc709619a8beca5d1
Eli-b-good <eli.i.henderson95@gmail.com>
2022-04-18 20:23:19 -0400

    UPDATE: added hashing methods

--- server/models/User.js
+++ server/models/User.js
@@ -63,3 +63,35 @@ const userSchema = mongoose.Schema(
     timestamps,
   }
 );
+
+//hash the password
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      this.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (matchPass) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(matchPass, this.password, (err, isMatch) => {
+      if (err) return reject(err);
+
+      if (!isMatch) return reject(false);
+
+      resolve(true);
+    });
+  });
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
