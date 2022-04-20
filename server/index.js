f3e61092fc7ea436cd809667f28c769805b43751
Eli-b-good <eli.i.henderson95@gmail.com>
2022-04-19 22:55:51 -0400

    heroku

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const axios = require("axios");
 const port = 4500;
 const app = express();
 
@@ -10,6 +11,27 @@ app.use(express.json());
 //routes
 app.get("/", (req, res) => {
   res.send("Server running");
+});
+
+app.post("/sku", async (req, res) => {
+  const { sku } = req.body;
+  try {
+    const { data } = await axios.post(
+      "https://api.upcitemdb.com/prod/trial/lookup",
+      `{ "upc": ${sku} }`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept-Encoding": "gzip",
+        },
+      }
+    );
+
+    return res.send(data);
+  } catch (error) {
+    console.log(error.message);
+    res.send(error);
+  }
 });
 
 app.listen(port, () => {
